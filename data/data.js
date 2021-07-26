6b9bf37936fd8a65136a4611cfcf1b0f1a8ec24b
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-26 13:21:19 -0700

    added questions 2 and 3

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 const sun = {
-    id: '1',
+    id: 1,
     title: 'The Sun',
-    question: `How many Earth's fit inside the sun?`,
+    question: `How many Earths fit inside the sun?`,
     correct: 4,
     choices: [{
         id: 1,
@@ -18,10 +18,50 @@ const sun = {
     }]
 };
 
+const mercury = {
+    id: 2,
+    title: 'Mercury',
+    question: `How many moons does Mercury have?`,
+    correct: 2,
+    choices: [{
+        id: 1,
+        answer: '5',
+    }, {
+        id: 2,
+        answer: '0',
+    }, {
+        id: 3,
+        answer: '3'
+    }, {
+        id: 4,
+        answer: '7'
+    }]
+};
 
+const venus = {
+    id: 3,
+    title: 'Venus',
+    question: `One day on Venus is equal to how many Earth days?`,
+    correct: 2,
+    choices: [{
+        id: 1,
+        answer: '100',
+    }, {
+        id: 2,
+        answer: '243',
+    }, {
+        id: 3,
+        answer: '325'
+    }, {
+        id: 4,
+        answer: '187'
+    }]
+};
 
 const planets = [
     sun, 
+    mercury,
+    venus
 ];
 
 export default planets;
