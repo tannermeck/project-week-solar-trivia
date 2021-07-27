33dbd7683f520475a377effd64f98aabee114c4e
TANNER MECK <steve.meck246@gmail.com>
2021-07-27 10:33:07 -0700

    ttd increment score, rendered questions and options, incremented getprofile score

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,1 @@
-import './example.test.js';
+import './storage-utils-tests.js';
--- trivia/trivia.js
+++ trivia/trivia.js
@@ -1,11 +1,34 @@
-import { getProfile } from '../storage-utils/storage-utils.js';
+import { getProfile, incrementScore } from '../storage-utils/storage-utils.js';
 import planets from '../data/data.js';
 import { getById } from '../storage-utils/helpers.js';
 
 getProfile();
 
+const question = document.getElementById('question');
 const answerForm = document.getElementById('answer-form');
 const searchParams = new URLSearchParams(window.location.search);
 
 const planet = getById(planets, +searchParams.get('planetId'));
-console.log(planet);
+question.textContent = planet.question;
+
+for (let choice of planet.choices){
+    const radio = document.createElement('input');
+    const span = document.createElement('span');
+    radio.type = 'radio';
+    radio.value = choice.id;
+    radio.name = 'answer';
+    span.textContent = choice.answer;
+    const label = document.createElement('label');
+    label.append(radio, span);
+    question.appendChild(label);
+}
+answerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selected = new FormData(answerForm);
+    const selection = selected.get('answer');
+    if (planet.correct === +selection){
+        incrementScore();
+    } else {
+        console.log('you-lose');
+    } getProfile();
+});
