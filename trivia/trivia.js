fa212f11d64008094d83d830e7995bfa332bfd94
Paul Brubaker <pbrubak@gmail.com>
2021-07-26 17:47:14 -0700

    add question and answer from

--- trivia/trivia.js
+++ trivia/trivia.js
@@ -1,3 +1,11 @@
 import { getProfile } from '../storage-utils/storage-utils.js';
+import planets from '../data/data.js';
+import { getById } from '../storage-utils/helpers.js';
 
 getProfile();
+
+const answerForm = document.getElementById('answer-form');
+const searchParams = new URLSearchParams(window.location.search);
+
+const planet = getById(planets, +searchParams.get('planetId'));
+console.log(planet);
