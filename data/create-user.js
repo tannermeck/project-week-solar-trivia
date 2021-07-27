5f008ef86166396a9b7bfcc281b5229138e33027
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-07-27 11:57:15 -0700

    added completed for links

--- data/create-user.js
+++ data/create-user.js
@@ -2,6 +2,7 @@ export function createUser(formData) {
     return {
         name: formData.get('name'),
         race: formData.get('race'),
-        score: 0
+        score: 0,
+        completed: [],
     };
 }
